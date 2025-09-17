04aa7a02c2a5f94f4edc0319baf0448f22839ca8
YmraNaissur <daily.spb@yandex.ru>
2025-09-17 22:11:11 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "ru.naissur"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
