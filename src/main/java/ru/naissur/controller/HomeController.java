b4a7fd69600df830603354717430eea352fba8e5
YmraNaissur <daily.spb@yandex.ru>
2025-09-17 22:21:28 +0300

    Remove exclamation marks

--- src/main/java/ru/naissur/controller/HomeController.java
+++ src/main/java/ru/naissur/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
   @GetMapping("/")
   public String home() {
-    return "Welcome to the Hexlet Spring Blog!!!";
+    return "Welcome to the Hexlet Spring Blog!";
   }
 
 }
