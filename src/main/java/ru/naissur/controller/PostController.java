96153bf79ea5da94eff780938fe2ebd64a1dabc9
YmraNaissur <daily.spb@yandex.ru>
2025-09-20 10:23:34 +0300

    Return appropriate HTTP codes

--- src/main/java/ru/naissur/controller/PostController.java
+++ src/main/java/ru/naissur/controller/PostController.java
@@ -4,6 +4,9 @@ import jakarta.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,38 +25,72 @@ public class PostController {
 
   List<Post> posts = new ArrayList<>();
 
-  @GetMapping("/")
-  public List<Post> getPosts() {
-    return posts;
+  @GetMapping()
+  public ResponseEntity<List<Post>> getPosts() {
+    if (CollectionUtils.isEmpty(posts)) {
+      return ResponseEntity
+          .notFound()
+          .build();
+    } else {
+      return ResponseEntity
+          .ok()
+          .header("X-Content-Length", String.valueOf(posts.size()))
+          .body(posts);
+    }
   }
 
   @GetMapping("/{id}")
-  public Optional<Post> getPostById(@PathVariable String id) {
-    return findById(id);
+  public ResponseEntity<Post> getPostById(@PathVariable String id) {
+    var post = findById(id);
+    if (post.isEmpty()) {
+      return ResponseEntity
+          .notFound()
+          .build();
+    } else {
+      return ResponseEntity.of(findById(id));
+    }
   }
 
-  @PostMapping("/")
-  public Post createPost(@Valid @RequestBody Post post) {
+  @PostMapping()
+  public ResponseEntity<Post> createPost(@Valid @RequestBody Post post) {
     posts.add(post);
-    return post;
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(post);
   }
 
   @PutMapping("/{id}")
-  public Post updatePost(@PathVariable String id, @Valid @RequestBody Post data) {
+  public ResponseEntity<Post> updatePost(@PathVariable String id, @Valid @RequestBody Post data) {
     var maybePost = findById(id);
-    if (maybePost.isPresent()) {
+    if (maybePost.isEmpty()) {
+      return ResponseEntity
+          .notFound()
+          .build();
+    } else {
       var post = maybePost.get();
       post.setTitle(data.getTitle());
       post.setAuthor(data.getAuthor());
       post.setCreatedAt(data.getCreatedAt());
       post.setContent(data.getContent());
+      return ResponseEntity
+          .ok()
+          .body(data);
     }
-    return data;
   }
 
   @DeleteMapping("/{id}")
-  public void deletePost(@PathVariable String id) {
-    posts.removeIf(p -> p.getTitle().equals(id));
+  public ResponseEntity<Void> deletePost(@PathVariable String id) {
+    var post = findById(id);
+    if (post.isEmpty()) {
+      return ResponseEntity
+          .notFound()
+          .build();
+    } else {
+      posts.removeIf(p -> p.getTitle().equals(id));
+      return ResponseEntity
+          .noContent()
+          .build();
+    }
   }
 
   private Optional<Post> findById(String id) {
