49ea8b1514005da8b1638471288eb271e7906bf1
YmraNaissur <daily.spb@yandex.ru>
2025-11-21 11:54:48 +0300

    Homework: filter products by price

--- src/main/java/ru/naissur/controller/ProductController.java
+++ src/main/java/ru/naissur/controller/ProductController.java
@@ -1,14 +1,22 @@
 package ru.naissur.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.naissur.exception.ResourceAlreadyExistsException;
 import ru.naissur.exception.ResourceNotFoundException;
 import ru.naissur.model.Product;
 import ru.naissur.repository.ProductRepository;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -16,6 +24,23 @@ public class ProductController {
 
   @Autowired
   private ProductRepository productRepository;
+
+  @GetMapping(path = "")
+  public List<Product> findFilteredByPrice(@RequestParam(required = false) Integer min, @RequestParam(required = false) Integer max) {
+    Sort sort = Sort.by("price").ascending();
+
+    if (min == null) {
+      if (max == null) {
+        return productRepository.findAll(sort);
+      } else {
+        min = 0;
+      }
+    } else if (max == null) {
+      max = Integer.MAX_VALUE;
+    }
+
+    return productRepository.findByPriceBetween(min, max, sort);
+  }
 
   @GetMapping(path = "/{id}")
   public Product findProductById(@PathVariable Long id) {
--- src/main/java/ru/naissur/repository/ProductRepository.java
+++ src/main/java/ru/naissur/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package ru.naissur.repository;
 
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.naissur.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  List<Product> findByPriceBetween(int startPrice, int endPrice, Sort sort);
 
 }
